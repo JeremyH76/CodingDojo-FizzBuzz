4e5753157cccb912226e0dac903bf8e368a1077a
Jérémy Hure <hure.j@sfeir.com>
2022-10-04 18:02:23 +0200

    Init : Stage 2

---
+++ CodingDojo-FizzBuzz/FizzBuzz2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo_FizzBuzz
+{
+    public static class FizzBuzz2
+    {
+        public static string Next(int i)
+        {
+            return Of(i + 1);
+        }
+
+        public static string Of(int i)
+        {
+            string ret = "";
+            if (i % 3 == 0) { ret += "Fizz"; }
+            if (i % 5 == 0) { ret += "Buzz"; }
+            if (ret == "") { ret = i.ToString(); }
+            return ret;
+        }
+    }
+}
---
+++ TestProject/UnitTest2.cs
@@ -0,0 +1,49 @@
+﻿using CodingDojo_FizzBuzz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void TestNext20First()
+        {
+            Assert.Equal("1", FizzBuzz2.Next(0));
+            Assert.Equal("2", FizzBuzz2.Next(1));
+            Assert.Equal("Fizz", FizzBuzz2.Next(2));
+            Assert.Equal("4", FizzBuzz2.Next(3));
+            Assert.Equal("Buzz", FizzBuzz2.Next(4));
+            Assert.Equal("Fizz", FizzBuzz2.Next(5));
+            Assert.Equal("7", FizzBuzz2.Next(6));
+            Assert.Equal("8", FizzBuzz2.Next(7));
+            Assert.Equal("Fizz", FizzBuzz2.Next(8));
+            Assert.Equal("Buzz", FizzBuzz2.Next(9));
+            Assert.Equal("11", FizzBuzz2.Next(10));
+            Assert.Equal("Fizz", FizzBuzz2.Next(11));
+            Assert.Equal("Fizz", FizzBuzz2.Next(12));
+            Assert.Equal("14", FizzBuzz2.Next(13));
+            Assert.Equal("FizzBuzz", FizzBuzz2.Next(14));
+            Assert.Equal("16", FizzBuzz2.Next(15));
+            Assert.Equal("17", FizzBuzz2.Next(16));
+            Assert.Equal("Fizz", FizzBuzz2.Next(17));
+            Assert.Equal("19", FizzBuzz2.Next(18));
+            Assert.Equal("Buzz", FizzBuzz2.Next(19));
+            Assert.Equal("Fizz", FizzBuzz2.Next(20));
+        }
+
+        [Fact]
+        public void TestBigNumber()
+        {
+            Assert.Equal("FizzFizzBuzz", FizzBuzz2.Of(300));
+            Assert.Equal("FizzFizzBuzz", FizzBuzz2.Of(300000000));
+            Assert.Equal("FizzFizzFizz", FizzBuzz2.Of(300000003));
+            Assert.Equal("Fizz", FizzBuzz2.Of(300000004));
+            Assert.Equal("FizzBuzzBuzz", FizzBuzz2.Of(300000005));
+        }
+
+    }
+}
