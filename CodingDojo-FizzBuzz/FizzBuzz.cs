677a93d97a750e0e6e7c8165bffd63cf5177a9e4
Jérémy Hure <hure.j@sfeir.com>
2022-10-04 17:27:12 +0200

    Test : Next20First

---
+++ CodingDojo-FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo_FizzBuzz
+{
+    public static class FizzBuzz
+    {
+        public static string Next(int i)
+        {
+            return "";
+        }
+    }
+}
--- TestProject/UnitTest.cs
+++ TestProject/UnitTest.cs
@@ -1,11 +1,33 @@
+using CodingDojo_FizzBuzz;
+
 namespace TestProject
 {
     public class UnitTest
     {
         [Fact]
-        public void Test1()
+        public void TestNext20First()
         {
-
+            Assert.Equal("1",FizzBuzz.Next(0));
+            Assert.Equal("2",FizzBuzz.Next(1));
+            Assert.Equal("Fizz",FizzBuzz.Next(2));
+            Assert.Equal("4",FizzBuzz.Next(3));
+            Assert.Equal("Buzz",FizzBuzz.Next(4));
+            Assert.Equal("Fizz",FizzBuzz.Next(5));
+            Assert.Equal("7",FizzBuzz.Next(6));
+            Assert.Equal("8",FizzBuzz.Next(7));
+            Assert.Equal("Fizz",FizzBuzz.Next(8));
+            Assert.Equal("Buzz",FizzBuzz.Next(9));
+            Assert.Equal("11",FizzBuzz.Next(10));
+            Assert.Equal("Fizz",FizzBuzz.Next(11));
+            Assert.Equal("13",FizzBuzz.Next(12));
+            Assert.Equal("14",FizzBuzz.Next(13));
+            Assert.Equal("FizzBuzz",FizzBuzz.Next(14));
+            Assert.Equal("16",FizzBuzz.Next(15));
+            Assert.Equal("17",FizzBuzz.Next(16));
+            Assert.Equal("Fizz",FizzBuzz.Next(17));
+            Assert.Equal("19",FizzBuzz.Next(18));
+            Assert.Equal("Buzz",FizzBuzz.Next(19));
+            Assert.Equal("Fizz",FizzBuzz.Next(20));
         }
     }
 }
