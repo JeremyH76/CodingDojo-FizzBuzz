5833fd3ea32ad838d1983bcb72e963f90258cbe8
Jérémy Hure <hure.j@sfeir.com>
2022-10-04 17:51:34 +0200

    Show numbers from 1 to 100

--- CodingDojo-FizzBuzz/Program.cs
+++ CodingDojo-FizzBuzz/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CodingDojo_FizzBuzz;
+
+int i = 0;
+while (i < 100)
+{
+    Console.WriteLine(FizzBuzz.Next(i));
+    i++;
+}
+
